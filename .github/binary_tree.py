c76f46d1bade7b01fd613ded514be2fdba2fcbed
luizcortez18 <luizcortezcasotti@edu.unifil.br>
2022-11-27 17:18:13 -0300

    arvore binaria

--- .github/binary_tree.py
+++ .github/binary_tree.py
@@ -11,21 +11,119 @@ class BinaryTree:
       self.root = node
     else:
       atual = self.root
-      while atual.esquerda != None or atual.direita != None:
-        if (node < atual.node):
-          atual = atual.setEsquerda()
+      while True:
+        ant = atual
+        if (value < atual.value):
+          atual = atual.esquerda
           if atual == None:
-            node.esquerda = node
+            ant.esquerda = node
+            break
         else:
-          atual = atual.setDireita()
+          atual = atual.direita
           if atual == None:
-            node.direita = node
+            ant.direita = node
+            break
 
   def emOrdem(self, atual):
     if atual != None:
       self.emOrdem(atual.esquerda)
-      print(atual.value)
+      print(atual.value, end = ' ')
       self.emOrdem(atual.direita)
 
+  def preOrdem(self, atual):
+    if atual != None:
+      print(atual.value, end = ' ')
+      self.preOrdem(atual.esquerda)
+      self.preOrdem(atual.direita)
+
+  def posOrdem(self, atual):
+    if atual != None:
+      self.posOrdem(atual.esquerda)
+      self.posOrdem(atual.direita)
+      print(atual.value, end = ' ')
+     
   def mostrar(self):
+    print("")
+    print("Em ordem: ")
     self.emOrdem(self.root)
+    print("")
+    print("Pré ordem: ")
+    self.preOrdem(self.root)
+    print("")
+    print("Pós ordem: ")
+    self.posOrdem(self.root)
+ 
+  def realocar(self, remove):
+    pai_s = remove
+    sucessor = remove
+    atual = remove.direita
+   
+    while atual != None:
+      pai_s = sucessor
+      sucessor = atual
+      atual = atual.esquerda
+
+    if sucessor != remove.direita:
+      pai_s.esquerda = sucessor.direita
+      sucessor.direita = remove.direita
+     
+    return sucessor
+
+  def remover(self, value):
+    if self.root == None:
+       print ("Árvore vazia.")
+
+    atual = self.root
+    pai = self.root
+    f_esq = True
+
+    while atual.value != value:
+      pai = atual
+      if value < atual.value:
+        atual = atual.esquerda
+        f_esq = True
+      else:
+        atual = atual.direita
+        f_esq = False
+      if atual == None:
+        return False
+       
+    if atual.esquerda == None and atual.direita == None:
+      if atual == self.root:
+        self.root = None
+      else:
+        if f_esq:
+          pai.esquerda = None
+        else:
+          pai.direita = None
+         
+    elif atual.esquerda == None:
+      if atual == self.root:
+        self.root = atual.direita
+      else:
+        if f_esq:
+          pai.esquerda = atual.direita
+        else:
+          pai.direita = atual.direita
+
+    elif atual.direita == None:
+      if atual == self.root:
+        self.root = atual.esquerda
+      else:
+        if f_esq:
+          pai.esquerda = atual.esquerda
+        else:
+          pai.direita = atual.esquerda
+
+    else:
+      sucessor = self.realocar(atual)
+      if atual == self.root:
+        self.root = sucessor
+      else:
+        if f_esq:
+          pai.esquerda = sucessor
+        else:
+          pai.direita = sucessor
+      sucessor.esquerda = atual.esquerda
+     
+    return True
--- .github/main.py
+++ .github/main.py
@@ -2,10 +2,33 @@ from binary_tree import BinaryTree
 
 arvore = BinaryTree()
 
-arvore.adicionar(4)
-arvore.adicionar(2)
-arvore.adicionar(3)
-arvore.adicionar(5)
-arvore.adicionar(7)
+arvore.adicionar(40)
+arvore.adicionar(20)
+arvore.adicionar(30)
+arvore.adicionar(60)
+arvore.adicionar(70)
+arvore.adicionar(10)
+arvore.adicionar(50)
+arvore.adicionar(35)
+arvore.adicionar(55)
+arvore.adicionar(75)
+arvore.adicionar(45)
+arvore.adicionar(65)
 
+arvore.mostrar()
+
+print("")
+arvore.remover(75)
+arvore.mostrar()
+
+print("")
+arvore.remover(50)
+arvore.mostrar()
+
+print("")
+arvore.remover(60)
+arvore.mostrar()
+
+print("")
+arvore.remover(20)
 arvore.mostrar()
